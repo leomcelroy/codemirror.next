f48340ffd2ce568cfc5b7456ea9e67b13cee8953
leomcelroy <leomcelroy@gmail.com>
2021-04-20 21:31:50 -0400

    added bundle

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,18 +1,17 @@
-import { EditorView } from "@codemirror/view";
 import { indentMore, indentLess } from "@codemirror/commands";
 import { javascript } from "@codemirror/lang-javascript";
-import {keymap, highlightSpecialChars, drawSelection, highlightActiveLine} from "@codemirror/view"
-import {Extension, EditorState} from "@codemirror/state"
-import {history, historyKeymap} from "@codemirror/history"
-import {foldGutter} from "@codemirror/fold"
-import {indentOnInput} from "@codemirror/language"
-import {lineNumbers} from "@codemirror/gutter"
-import {bracketMatching} from "@codemirror/matchbrackets"
-import {closeBrackets, closeBracketsKeymap} from "@codemirror/closebrackets"
-import {highlightSelectionMatches, selectNextOccurrence} from "@codemirror/search"
-import {commentKeymap} from "@codemirror/comment"
-import {rectangularSelection} from "@codemirror/rectangular-selection"
-import {defaultHighlightStyle} from "@codemirror/highlight"
+import { EditorView, keymap, highlightSpecialChars, drawSelection, highlightActiveLine } from "@codemirror/view"
+import { Extension, EditorState } from "@codemirror/state"
+import { history, historyKeymap } from "@codemirror/history"
+import { foldGutter } from "@codemirror/fold"
+import { indentOnInput } from "@codemirror/language"
+import { lineNumbers } from "@codemirror/gutter"
+import { bracketMatching } from "@codemirror/matchbrackets"
+import { closeBrackets, closeBracketsKeymap } from "@codemirror/closebrackets"
+import { highlightSelectionMatches, selectNextOccurrence } from "@codemirror/search"
+import { commentKeymap } from "@codemirror/comment"
+import { rectangularSelection } from "@codemirror/rectangular-selection"
+import { defaultHighlightStyle } from "@codemirror/highlight"
 
 
 const tabBinding = {key: "Tab", run: indentMore, shift: indentLess}
@@ -63,8 +62,6 @@ class CodeMirror extends HTMLElement {
 		});
 	}
 
-
-
 	// lifecycle
 	connectedCallback() { 
 		new EditorView({ state: this.state, parent: this });
@@ -74,24 +71,9 @@ class CodeMirror extends HTMLElement {
 
 window.customElements.define("code-mirror", CodeMirror);
 
-
-let state = EditorState.create({ doc: `const {readFile} = require("fs");
-readFile("package.json", "utf8", (err, data) => {
-console.log(data);
-});
-`, extensions: [
-        basicSetup,
-        // oneDark,
-        // keymap.of([defaultTabBinding]),
-        javascript(),
-        //  linter(esLint(new Linter)),
-        //  StreamLanguage.define(javascript),
-    ] });
-
-new EditorView({ state, parent: document.querySelector("#editor") });
-
-
-
+// window.addEventListener("load", () => {
+// 	window.customElements.define("code-mirror", CodeMirror);
+// })
 
 
 
